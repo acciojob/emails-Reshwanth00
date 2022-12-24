f4b83379b62d7dcfeca25a0d5b3a15b191408c2d
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2022-12-24 18:15:14 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -17,41 +17,41 @@ public class Email {
     public String getPassword() {
         return password;
     }
-    public boolean doesUpper(String str){
-        for(int i=0;i<str.length();i++){
-            if(Character.isUpperCase(str.charAt(i))){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean doesLower(String str){
-        for(int i=0;i<str.length();i++){
-            if(Character.isLowerCase(str.charAt(i))){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean doesDigit(String str){
-        for(int i=0;i<str.length();i++){
-            if(Character.isDigit(str.charAt(i))){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean doesSpecial(String str){
-        for(int i=0;i<str.length();i++){
-            if(!Character.isAlphabetic(str.charAt(i)) && !Character.isDigit(str.charAt(i))){
-                return true;
-            }
-        }
-        return false;
-    }
+
     public void changePassword(String oldPassword, String newPassword){
-        if(oldPassword.equals(this.password) && newPassword.length()>7 && doesUpper(newPassword) && doesLower(newPassword) && doesDigit(newPassword) && doesSpecial(newPassword)){
-            this.password=newPassword;
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword))
+        {
+            if(newPassword.length()>=8)
+            {
+                int up=0;
+                int lo=0;
+              int digit=0;
+              int sp=0;
+                for(int i =0 ; i <newPassword.length();i++)
+                {
+                    char ch =newPassword.charAt(i);
+                    if(ch >='A' && ch <='Z')
+                        up++;
+                    else if(ch>='a' && ch<='z')
+                        lo++;
+                    else if(ch>='0' && ch <='9')
+                        digit++;
+                    else
+                        sp++;
+                }
+                if(up >=1 && lo>=1 && digit >=1 && sp>=1)
+                {
+                   this.password=newPassword;
+                }
+
+            }
+
         }
     }
 }
