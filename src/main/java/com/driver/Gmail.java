fdcc33bdc0e66693adf4b5f2a8e63c95559ca291
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2022-12-24 18:16:07 +0530

    Update Gmail.java

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -1,99 +1,57 @@
 package com.driver;
 
-import java.util.*;
+public class Email {
 
-public class Gmail extends Email {
+    private String emailId;
+    private String password;
 
-    private int inboxCapacity;
-    public Gmail(String emailId, int inboxCapacity) {
-        super(emailId);
-        this.inboxCapacity=inboxCapacity;
+    public Email(String emailId){
+        this.emailId = emailId;
+        this.password = "Accio@123";
     }
 
-    private Deque<node> inbox = new ArrayDeque<>();
-    public void receiveMail(Date date, String sender, String message){
-        node temp = new node(date,sender,message);
-        if(this.inboxCapacity== inbox.size()){
-            this.inbox.addLast(temp);
-            this.inbox.removeFirst();
-        }
-        else{
-            this.inbox.addLast(temp);
-        }
+    public String getEmailId() {
+        return emailId;
     }
-    private Deque<node> trash = new ArrayDeque<>();
-    public void deleteMail(String message){
-        Deque<node> temp = new ArrayDeque<>();
-        while(!this.inbox.isEmpty()){
-            if(!this.inbox.peekFirst().message.equals(message)){
-                temp.addLast(this.inbox.peek());
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void changePassword(String oldPassword, String newPassword){
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword))
+        {
+            if(newPassword.length()>=8)
+            {
+                int up=0;
+                int lo=0;
+              int digit=0;
+              int sp=0;
+                for(int i =0 ; i <newPassword.length();i++)
+                {
+                    char ch =newPassword.charAt(i);
+                    if(ch >='A' && ch <='Z')
+                        up++;
+                    else if(ch>='a' && ch<='z')
+                        lo++;
+                    else if(ch>='0' && ch <='9')
+                        digit++;
+                    else
+                        sp++;
+                }
+                if(up >=1 && lo>=1 && digit >=1 && sp>=1)
+                {
+                   this.password=newPassword;
+                }
+
             }
-            else{
-                trash.addLast(this.inbox.peek());
-            }
-            this.inbox.removeFirst();
+
         }
-        this.inbox=temp;
-    }
-
-    public String findLatestMessage(){
-        if(this.inbox.size()==0){
-            return "null";
-        }
-        else {
-            return this.inbox.getLast().message;
-        }
-    }
-
-    public String findOldestMessage(){
-        if(inbox.size()==0){
-            return null;
-        }
-        else {
-            return this.inbox.getFirst().message;
-        }
-    }
-
-    public int findMailsBetweenDates(Date start, Date end){
-        Deque<node> temp = new ArrayDeque<>();
-        int count = 0;
-        while(!this.inbox.isEmpty()){
-            if(this.inbox.getFirst().date.compareTo(start)>=0 && this.inbox.getFirst().date.compareTo(end)<=0){
-                count ++;
-            }
-            temp.addFirst(this.inbox.getFirst());
-            this.inbox.removeFirst();
-        }
-        this.inbox=temp;
-        return count;
-    }
-
-    public int getInboxSize(){
-        return this.inbox.size();
-
-    }
-
-    public int getTrashSize(){
-        return this.trash.size();
-
-    }
-
-    public void emptyTrash(){
-        Deque<node> temp = new ArrayDeque<>();
-        this.trash=temp;
-    }
-
-    public int getInboxCapacity() {
-        return this.inbox.size();
-    }
-}
-class node{
-    Date date;
-    String sender;
-    String message;
-    public node(Date date,String sender,String message){
-        this.date=date;
-        this.sender=sender;
-        this.message=message;
     }
 }
