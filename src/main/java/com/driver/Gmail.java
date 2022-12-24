859a2a310d34b2d085d70b9ea558aa79510ed44f
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2022-12-24 17:25:17 +0530

    Update Gmail.java

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -1,65 +1,99 @@
 package com.driver;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 public class Gmail extends Email {
 
-    int inboxCapacity; //maximum number of mails inbox can store
-    //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
-    //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
+    private int inboxCapacity;
     public Gmail(String emailId, int inboxCapacity) {
-
+        super(emailId);
+        this.inboxCapacity=inboxCapacity;
     }
 
+    private Deque<node> inbox = new ArrayDeque<>();
     public void receiveMail(Date date, String sender, String message){
-        // If the inbox is full, move the oldest mail in the inbox to trash and add the new mail to inbox.
-        // It is guaranteed that:
-        // 1. Each mail in the inbox is distinct.
-        // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
+        node temp = new node(date,sender,message);
+        if(this.inboxCapacity== inbox.size()){
+            this.inbox.addLast(temp);
+            this.inbox.removeFirst();
+        }
+        else{
+            this.inbox.addLast(temp);
+        }
     }
-
+    private Deque<node> trash = new ArrayDeque<>();
     public void deleteMail(String message){
-        // Each message is distinct
-        // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
-
+        Deque<node> temp = new ArrayDeque<>();
+        while(!this.inbox.isEmpty()){
+            if(!this.inbox.peekFirst().message.equals(message)){
+                temp.addLast(this.inbox.peek());
+            }
+            else{
+                trash.addLast(this.inbox.peek());
+            }
+            this.inbox.removeFirst();
+        }
+        this.inbox=temp;
     }
 
     public String findLatestMessage(){
-        // If the inbox is empty, return null
-        // Else, return the message of the latest mail present in the inbox
-
+        if(this.inbox.size()==0){
+            return "null";
+        }
+        else {
+            return this.inbox.getLast().message;
+        }
     }
 
     public String findOldestMessage(){
-        // If the inbox is empty, return null
-        // Else, return the message of the oldest mail present in the inbox
-
+        if(inbox.size()==0){
+            return "null";
+        }
+        else {
+            return this.inbox.getFirst().message;
+        }
     }
 
     public int findMailsBetweenDates(Date start, Date end){
-        //find number of mails in the inbox which are received between given dates
-        //It is guaranteed that start date <= end date
-
+        Deque<node> temp = new ArrayDeque<>();
+        int count = 0;
+        while(!this.inbox.isEmpty()){
+            if(this.inbox.getFirst().date.compareTo(start)>=0 && this.inbox.getFirst().date.compareTo(end)<=0){
+                count ++;
+            }
+            temp.addFirst(this.inbox.getFirst());
+            this.inbox.removeFirst();
+        }
+        this.inbox=temp;
+        return count;
     }
 
     public int getInboxSize(){
-        // Return number of mails in inbox
+        return this.inbox.size();
 
     }
 
     public int getTrashSize(){
-        // Return number of mails in Trash
+        return this.trash.size();
 
     }
 
     public void emptyTrash(){
-        // clear all mails in the trash
-
+        Deque<node> temp = new ArrayDeque<>();
+        this.trash=temp;
     }
 
     public int getInboxCapacity() {
-        // Return the maximum number of mails that can be stored in the inbox
+        return this.inbox.size();
+    }
+}
+class node{
+    Date date;
+    String sender;
+    String message;
+    public node(Date date,String sender,String message){
+        this.date=date;
+        this.sender=sender;
+        this.message=message;
     }
 }
